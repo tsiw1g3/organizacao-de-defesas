08c407c1a41bcad9344d2d56716e31e5a7b4689d
bfs07 <bfs.07@hotmail.com>
2021-09-08 13:53:12 -0300

    add back-end stuff

---
+++ back-end/config/db.php
@@ -0,0 +1,10 @@
+<?php
+// Created By @hoaaah * Copyright (c) 2020 belajararief.com
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=moon',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];

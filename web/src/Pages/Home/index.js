dc3d973d2b4b9bae7c3288f0ac4a6f1effc3f326
Tom Dias <antoniofdiass@gmail.com>
2021-09-19 21:34:51 -0300

    chore: reupload front-end

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -8,20 +8,51 @@ import { Button, Paper } from "@material-ui/core";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
+const ptBrLocale = {
+  code: "pt-br",
+  week: {
+    dow: 0, // Sunday is the first day of the week.
+    doy: 6, // The week that contains Jan 1st is the first week of the year.
+  },
+  buttonText: {
+    prev: "Ant",
+    next: "Sig",
+    today: "Hoje",
+    month: "Mes",
+    week: "Semana",
+    day: "Día",
+    list: "Agenda",
+  },
+  weekText: "Sm",
+  allDayText: "Todo el día",
+  moreLinkText: "más",
+  noEventsText: "No hay eventos para mostrar",
+};
+
 function Home() {
   return (
     <Container maxWidth="sm" className="App">
       <Paper>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        <FullCalendar
+          locales={[ptBrLocale]}
+          locale="pt-br"
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          weekends={false}
+          events={[
+            { title: "Apresentação Fred", date: "2021-09-13" },
+            { title: "Próxima aula", date: "2021-09-20" },
+          ]}
+        />
         {/* <img src={logo} className="App-logo" alt="logo" />
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App + Material-UI
         </Typography> */}
         <Button variant="contained" color="primary">
-          Primary Button
+          Botão 1
         </Button>
         <Button variant="contained" color="secondary">
-          Secondary Button
+          Botão 2
         </Button>
       </Paper>
     </Container>

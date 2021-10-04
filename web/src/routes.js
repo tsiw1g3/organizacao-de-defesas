74ef034e7433991f20ff6867009146dd32440dc2
Tom Dias <antoniofdiass@gmail.com>
2021-10-04 08:53:19 -0300

    feat(board) add default form

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./Pages/Home";
+import ExaminingBoard from "./Pages/ExaminingBoard";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route component={Home} path="/" exact />
+      <Route component={ExaminingBoard} path="/banca/criar" />
     </BrowserRouter>
   );
 };
